7d769ae1358bb24875c446347bfe3d952e723b11
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-03-23 12:36:15 -0400

    moved index.html and update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {fileURLToPath} from 'url';
+import {dirname, resolve} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = dirname(__filename);
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quick-links/',
+  build: {
+    outDir,
+
+    emptyOutDir: true,
+    rollupOptions: {
+
+      input: {
+        index: resolve(root,  'index.html'),
+       
+      }
+    }
+  }
 })
